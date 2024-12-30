2f779c59d0c6d71c6c035e856118a1704d798b1b
sergioivanov008 <sergioivanov008@gmail.com>
2024-12-30 19:14:39 +0300

    feat: add starting design for ContentTopRight component

--- src/pages/main/ui/content-top-right/ContentTopRight.tsx
+++ src/pages/main/ui/content-top-right/ContentTopRight.tsx
@@ -1,7 +1,23 @@
-import styles from './ContentTopRight.module.scss';
+import Image from 'next/image';
+import imgMain from '@/shared/assets/main/foto003.png';
+import s from './ContentTopRight.module.scss';
 
 export function ContentTopRight() {
   return (
-    <div className={styles.contentTopRight}>contentTopRight</div>
+    <div className={s.contentTopRight}>
+      <Image
+        src={imgMain}
+        alt=''
+        className={s.mainImage}
+      />
+      <div className={s.blockText}>
+        <div className={s.textSmall}>Concert</div>
+        <div className={s.textBig}>Salaskbron</div>
+      </div>
+      <div className={s.calendar}>
+        <div className={s.month}>oct</div>
+        <div className={s.date}>29</div>
+      </div>
+    </div>
   )
 }
