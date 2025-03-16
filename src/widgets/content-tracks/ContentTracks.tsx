abadfe32ae7f10e08b861e031eda47bd1d533ef0
sergioivanov008 <sergioivanov008@gmail.com>
2025-03-17 00:11:53 +0300

    feat(app): большой коммит
    большой коммит

--- src/widgets/content-tracks/ContentTracks.tsx
+++ src/widgets/content-tracks/ContentTracks.tsx
@@ -1,85 +1,16 @@
-import { TrackItem, TrackItemType } from '@/shared/ui/track-item/TrackItem';
+import { TrackItem } from '@/shared/ui/track-item/TrackItem';
 import s from './ContentTracks.module.scss';
-import trackItemImg from '@/shared/assets/images/track_item_01.png';
 import { ContentTitle } from '@/shared/ui';
-
-const CONTENT_TRACKS = {
-	title: {
-		left: 'Tracks of the Day',
-		right: 'See all',
-	},
-};
-
-const TRACK_ITEM_TITLE: TrackItemType = {
-	number: '№',
-	cover: {
-		title: 'Cover',
-	},
-	name: {
-		title: 'Name',
-	},
-	album: 'Album',
-	listens: 'Listens',
-	playTime: 'Play time',
-	like: '',
-	isTitle: true,
-};
-
-const TRACK_ITEMS: TrackItemType[] = [
-	{
-		number: '1',
-		cover: {
-			title: 'Album cover',
-			cover: trackItemImg,
-		},
-		name: {
-			title: 'Los del Espasio',
-			author: 'Rusherking',
-		},
-		album: 'El Espasio',
-		listens: '213',
-		playTime: '5:38',
-		like: '',
-	},
-	{
-		number: '2',
-		cover: {
-			title: 'Album cover',
-			cover: trackItemImg,
-		},
-		name: {
-			title: 'Los del Espasio',
-			author: 'Rusherking',
-		},
-		album: 'El Espasio',
-		listens: '213',
-		playTime: '5:38',
-		like: '',
-	},
-	{
-		number: '3',
-		cover: {
-			title: 'Album cover',
-			cover: trackItemImg,
-		},
-		name: {
-			title: 'Los del Espasio',
-			author: 'Rusherking',
-		},
-		album: 'El Espasio',
-		listens: '213',
-		playTime: '5:38',
-		like: '',
-	},
-];
+import {
+	ContentTracksData,
+	TRACK_ITEM_TITLE,
+	TRACK_ITEMS,
+} from '@/shared/constants/constants';
 
 export const ContentTracks = () => {
 	return (
 		<div className={s.wrapper}>
-			<ContentTitle
-				left={CONTENT_TRACKS.title.left}
-				right={CONTENT_TRACKS.title.right}
-			/>
+			<ContentTitle data={ContentTracksData} />
 			<div className={s.contentMain}>
 				<TrackItem data={TRACK_ITEM_TITLE} />
 				<div className={s.contentItems}>
